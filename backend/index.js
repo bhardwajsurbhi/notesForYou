9aca7a91ffdab46574c030404af52833d24ab118
bhardwajsurbhi <103811303+bhardwajsurbhi@users.noreply.github.com>
2022-07-05 10:40:17 +0530

    database

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,20 @@
-const { connect } = require('mongoose');
-const connectToMongo = require('./db');
+// const { connect } = require('mongoose');
+const mongoose = require('mongoose');
+// const connectToMongo = require('./db');
 const express = require('express')
 var cors = require('cors')
+const DB = 'mongodb+srv://notesForYouDB:iejADU91AZucwca9@cluster0.unof7xn.mongodb.net/?retryWrites=true&w=majority';
 
 
-connectToMongo();
+mongoose.connect(DB).then(() => {
+  console.log(`connection successful with notesForYouDB`);
+}).catch((err) => console.log(`no connection`));
+
+
+// connectToMongo();
 const app = express()
-const port = 5000
+// const port = 5000
+const port = process.env.PORT || 5000;
 
 app.use(cors())
 app.use(express.json())
