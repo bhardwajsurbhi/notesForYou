c0c99d4efa1af8b8f395d6ce5e8bb601d23fe04c
bhardwajsurbhi <103811303+bhardwajsurbhi@users.noreply.github.com>
2022-07-06 10:13:30 +0530

    heroku postbuild

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,11 +1,11 @@
 const mongoose =require('mongoose');
 const {Schema} = mongoose;
-const DB = 'mongodb+srv://notesForYouDB:iejADU91AZucwca9@cluster0.unof7xn.mongodb.net/?retryWrites=true&w=majority';
+// const DB = 'mongodb+srv://notesForYouDB:iejADU91AZucwca9@cluster0.unof7xn.mongodb.net/?retryWrites=true&w=majority';
 
 
-mongoose.connect(DB).then(() => {
-  console.log(`connection successful`);
-}).catch((err) => console.log(`no connection`));
+// mongoose.connect(DB).then(() => {
+//   console.log(`connection successful`);
+// }).catch((err) => console.log(`no connection`));
 
 
 //creating schema for database
